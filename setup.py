715f3c6aa320c94cc82ffa09627aa608f6df6502
sahana-github <1ms20is407@gmail.com>
2025-05-25 11:27:36 +0530

    project structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sahana",
+    author_email="1ms20is407gmail.com",
+    packages=find_packages()
+)
